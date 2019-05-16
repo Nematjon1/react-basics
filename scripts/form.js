6d067a289de8b7c1e7cbcd2f793a02edc3f5c539
Nematjon <frank.sport.93@gmail.com>
2019-05-16 15:30:59 +0300

    Reservation Component

--- scripts/form.js
+++ scripts/form.js
@@ -30,3 +30,40 @@ class NameForm extends Component {
   }
 }
 
+class Reservation extends React.Component {
+  state = {
+    isGoing = true,
+    numberOfGuests: 2
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ?
+      target.checked : target.value;
+    const name = target.name;
+    this.setState({ [name]: value})
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of Guests:
+          <input
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+      </form>
+    );
+  }
+}
