f1fa6d966c8cc7b78c63519ca46f7551059e6d21
Nematjon <frank.sport.93@gmail.com>
2019-05-16 10:18:48 +0300

    added props

--- scripts/state.js
+++ scripts/state.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 class MyComponent extends React.Component {
   state = {
@@ -14,3 +15,46 @@ class MyComponent extends React.Component {
   }
 }
 export default MyComponent;
+
+// **********
+tick() {
+  const element = (
+    <div>
+      <h2>Hello, World!</h2>
+      <h3>{new Date().toLocaleTimString()}</h3>
+    </div>
+  );
+
+  return element;
+}
+
+class Clock extends React.Component {
+  state = {
+    date: new Date()
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      return this.tick();
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({date: new Date()});
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Hello</h2>
+        <p>It is {this.state.date.toLocaleTimeString()}</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById("app"));
