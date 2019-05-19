33a0dae9fd9c9aad34de37047bc4ee98cfc4a61a
Nematjon <frank.sport.93@gmail.com>
2019-05-19 10:30:33 +0300

    state_lifting added

---
+++ scripts/state_lifting.js
@@ -0,0 +1,94 @@
+import React, {Component} from "react";
+
+const BoilingVerdict = (props) => {
+  if(props.celsius >= 100) {
+    return <p>The water would boil.</p>
+  }
+
+  return <p>The water would not boil.</p>
+};
+
+class Calculator extends Component {
+  state = {
+    temperature: "",
+    scale: "c"
+  }
+
+  handleCelsiusChange = (temperature) => {
+    this.setState({ scale: "c", temperature})
+  }
+  handleFahrenheitChange = (temperature) => {
+    this.setState({scale: "f", temperature});
+  }
+
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius = scale === "f" ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature;
+
+    return (
+      <Temperature
+        scale="c"
+        onTemperatureChange={this.handleCelsiusChange}
+        temperature={celsius} />
+      <Temperature
+        onTemperatureChange={this.handleFahrenheitChange}
+        scale="f"
+        temperature={fahrenheit} />
+      <BoilingVerdict
+        celsius={parseFloat(temperature)} />
+    );
+  }
+}
+
+class TemperatureInput extends Component {
+
+  handleChange = (event) => {
+    // this.setState({temperature: event.target.value});
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input
+          value={temperature}
+          onChange={this.handleChange}
+        />
+      </fieldset>
+    );
+  }
+}
+
+const Calculator = (props) => (
+  <div>
+    <Temperature scale="c" />
+    <Temperature scale="f" />
+  </div>
+);
+
+const toCelsius = (fahrenheit) => {
+  return (fahrenheit - 32) * 5 / 9;
+};
+
+const toFahrenheit = (celsius) => {
+  return (celsius * 9 / 5) + 32;
+};
+
+const tryConvert = (temperature, convert) => {
+  const input = parseFloat(temperature);
+
+  if(Number.isNaN(input)) {
+    return "";
+  }
+
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+
+  return rounded.toString();
+};
