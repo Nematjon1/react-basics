4e900483d24e34cf0e0851243cc8d9aeb799604f
Nematjon <frank.sport.93@gmail.com>
2019-05-20 11:18:26 +0300

    Updated composition.js file

--- scripts/composition.js
+++ scripts/composition.js
@@ -18,3 +18,43 @@ const WelcomeDialog = () => {
     </FancyBorder>
   );
 };
+
+// Different approach
+const SplitPane = (props) => {
+  return (
+    <div className="SplitPane">
+      <div className="SplitPane-left">
+        {props.left}
+      </div>
+      <div className="SplitPane-right">
+        {props.right}
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <SplitPane
+      left={<Contacts />}
+      right={<Chat />}
+    />
+  );
+}
+
+const Dialog = (props) => {
+  return (
+    <FancyBorder color="blue">
+      <h2 className="Dialog-title">{props.title}</h2>
+      <p className="Dialog-message">{props.message}</p>
+      {props.children}
+    </FancyBorder>
+  );
+};
+
+const WelcomeDialog = () => (
+  <Dialog
+    title="Welcome"
+    message="Thank you for vsiting."
+  />
+);
