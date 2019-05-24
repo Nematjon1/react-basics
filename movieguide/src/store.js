7d07f5199a82946c848a32edbae36ab9f3caebb4
Nematjon <frank.sport.93@gmail.com>
2019-05-24 11:37:15 +0300

    store.js added

---
+++ movieguide/src/store.js
@@ -0,0 +1,9 @@
+import { createStore } from "redux":
+import { devToolsEnhancer } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+
+export default createStore(
+  rootReducer,
+  devToolsEnhancer()
+);
+
