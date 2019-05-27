8cf2953afa29d12de860e87d937ac65f67d732a4
Nematjon <frank.sport.93@gmail.com>
2019-05-27 19:22:42 +0300

    App.js added

---
+++ searchable/src/components/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import SearchBar from "./SearchBar/SearchBar";
+import ProductTable from "./ProductTable/ProductTable";
+import './App.css';
+
+class App extends Component {
+  state = {
+    filterText: "",
+    inStockOnly: false
+  }
+
+  handleFilterTextChange = (filterText) => {
+    this.setState({
+      filterText
+    });
+  }
+
+  handleInStockChange = (inStockOnly) => {
+    this.setState({
+      inStockOnly
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBar
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
+          filterText={this.state.filterText}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStock={this.state.inStockOnly}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
