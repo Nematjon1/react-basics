2d1e0694d5f2fa06691c5574e7d3eb9322b4c65f
Nematjon <frank.sport.93@gmail.com>
2019-06-16 12:36:51 +0300

    useRefHook file added

---
+++ scripts/useRefHook.js
@@ -0,0 +1,15 @@
+function Timer() {
+  const intervalRef = useRef();
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      // ...
+    });
+    intervalRef.current = id;
+    return () => {
+      clearInterval(intervalRef.current);
+    }
+  });
+
+  // ...
+}
