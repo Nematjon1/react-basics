4c5a6eaddfd4bb5afe5ee4d3f9d789e2b7f8c3d4
Nematjon <frank.sport.93@gmail.com>
2019-06-17 11:54:16 +0300

    Ajax call with fetch API

---
+++ scripts/ajaxExample.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from "react";
+
+const MyComponent = (props) => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.example.com/items")
+      .then(res => res.json())
+      .then(result => {
+        setIsLoaded(true);
+        setItems(result.items)
+      }, error => {
+        setError(error);
+        setIsLoaded(true)
+      })
+  })
+
+  if(error) <div>Error: {error.message}</div>;
+  else if (!isLoaded) <div>Loading...</div>;
+  else {
+    return (
+      <ul>
+        {items.map(item => {
+          <li key={item.id}>
+            {item.name} {item.price}
+          </li>
+        })}
+      </ul>
+    );
+  }
+};
