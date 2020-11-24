8ba0299474cc6a1e2f62dcd2566b75296e3c1ec4
Nematjon <frank.sport.93@gmail.com>
2020-11-24 14:46:17 +0300

    Added some useful comments and updated some code.

--- scripts/fragment.js
+++ scripts/fragment.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {Fragment} from "react";
+
+/**
+  * <Fragment></Fragment> or <></>
+  * Either will do
+**/
 
 const Glossary = (props) => {
   return (
     <dl>
       {props.items.map(item => {
         // Without the key, React will fire a key warning
-        <React.Fragment key={item.id}>
+       // It should be unique value
+        <Fragment key={item.id}>
           <dt>{item.term}</dt>
           <dd>{item.description}</dd>
-        </React.Fragment>
+        </Fragment>
       })}
     </dl>
   );
-}
+};
 
