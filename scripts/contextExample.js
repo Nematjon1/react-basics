a7e5ee3fb8a0d8a1db894aacc71c75e3d918a600
Nematjon <frank.sport.93@gmail.com>
2020-11-25 11:53:55 +0300

    context example

---
+++ scripts/contextExample.js
@@ -0,0 +1,36 @@
+// theme-context.js
+
+export const themes = {
+  light: {
+    foreground: '#000000',
+    background: '#eeeeee',
+  },
+  dark: {
+    foreground: '#ffffff',
+    background: '#222222',
+  },
+};
+
+export const ThemeContext = React.createContext(
+  themes.dark // default value
+);
+
+// themed-button.js
+
+import {ThemeContext} from './theme-context';
+
+class ThemedButton extends React.Component {
+  render() {
+    let props = this.props;
+    let theme = this.context;
+    return (
+      <button
+        {...props}
+        style={{backgroundColor: theme.background}}
+      />
+    );
+  }
+}
+ThemedButton.contextType = ThemeContext;
+
+export default ThemedButton;
